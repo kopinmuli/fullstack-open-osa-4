ad77367446231817272f1caaeb172d0f44e9afee
Kopinmuli <samulikoppigmail.com>
2022-03-16 10:16:20 +0200

    poistettu virhe

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -4,15 +4,6 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
-
   blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 })
